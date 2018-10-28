bec443663ba52ef02262a2a013328779380f2714
Dawid24 <paciorekdawidonly@gmail.com>
2018-10-28 11:35:32 +0100

    add a model and index page

---
+++ src/main/java/pl/paciorek/dawid/blogApplication/model/Post.java
@@ -0,0 +1,18 @@
+package pl.paciorek.dawid.blogApplication.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date added = new Date();
+}
