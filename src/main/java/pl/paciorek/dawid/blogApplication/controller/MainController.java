184fecef5675f728b97cabf38dbaa94de2c021ed
Dawid24 <paciorekdawidonly@gmail.com>
2018-10-28 11:52:50 +0100

    add thymeleaf

--- src/main/java/pl/paciorek/dawid/blogApplication/controller/MainController.java
+++ src/main/java/pl/paciorek/dawid/blogApplication/controller/MainController.java
@@ -1,13 +1,15 @@
 package pl.paciorek.dawid.blogApplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+        model.addAttribute("name", "Dawid");
         return "index";
     }
 }
